34dc385929bcfecdd213dee5da11d13c1c86153c
Vinod Tambe <128139036+vinodtambe007@users.noreply.github.com>
2025-02-19 16:27:22 +0530

    Update Navbar.js

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { FaCartArrowDown } from "react-icons/fa";
 import { ImHome } from "react-icons/im";
